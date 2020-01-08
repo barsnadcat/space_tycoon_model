6a0a4fc13c4861ede5f70a67787e97460da20d65
barsandcat <barsandcat@Merinda.Merinda>
2020-01-08 23:26:04 +0200

    Add RemoveDead function

--- src/IEntity.h
+++ src/IEntity.h
@@ -5,6 +5,20 @@ class IEntity
 public:
 	IEntity() = default;
 	virtual ~IEntity() = default;
+	virtual uint8_t GetHealth() const { return 0; }
+	virtual void SetHealth(uint8_t health) {}
 
 	virtual void Update() {}
 };
+
+template<typename T>
+void RemoveDead(std::vector<std::shared_ptr<T>>& inventory)
+{
+	inventory.erase(
+		std::remove_if(inventory.begin(), inventory.end(),
+			[](std::shared_ptr<T> ptr)
+			{
+				return !ptr->GetHealth();
+			}),
+		inventory.end());
+}
--- tests/EntityVectorUpdate.Test.cpp
+++ tests/EntityVectorUpdate.Test.cpp
@@ -6,4 +6,11 @@
 TEST(EntityVector, Udpate) 
 {
 	// Dead entities have to be removed from vector.
+	std::vector<std::shared_ptr<Food>> inventory;
+	std::shared_ptr<Food> food = std::make_shared<Food>();
+	food->SetHealth(0);
+	inventory.push_back(food);
+	food.reset();
+	RemoveDead<Food>(inventory);
+	EXPECT_TRUE(inventory.empty());
 }
