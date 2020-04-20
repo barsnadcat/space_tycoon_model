02da71113e00697d41f00276eeb4f1241711f519
Dmitriy Tsahilov <Dmitriy.Ts@fraglab.local>
2020-04-20 13:47:58 +0300

    Fix DecayMaxHealth coud put max health below 0

--- src/IEntity.h
+++ src/IEntity.h
@@ -19,7 +19,13 @@ public:
 	uint32_t GetMaxHealth() const { return mMaxHealth; }
 	void SetHealth(uint32_t health) { mHealth = std::min(health, mMaxHealth); }
 	void SetMaxHealth(uint32_t health) { mMaxHealth = health; SetHealth(mHealth); }
-	void DecayMaxHealth(uint32_t days) { mMaxHealth = mMaxHealth - days; mHealth = mHealth - days; }
+	void DecayMaxHealth(uint32_t days)
+	{
+		if (mMaxHealth > 0)
+		{
+			SetMaxHealth(mMaxHealth - days);
+		}
+	}
 private:
 	uint32_t mHealth { 0 };
 	uint32_t mMaxHealth { 0 };
