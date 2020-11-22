0a5a3ccd5612006864439e6f00482c3658985f2a
Dmitriy Tsahilov <Dmitriy.Ts@fraglab.local>
2020-11-22 23:12:51 +0200

    Firs BestProductionTest, and its failing

--- tests/BestProduction.Test.cpp
+++ tests/BestProduction.Test.cpp
@@ -2,11 +2,17 @@
 #include <gtest/gtest.h>
 
 #include <Person.h>
-
+#include <UpdateContext.h>
+#include <ObjectiveUtilitiesConfig.h>
+#include <ProductionsConfig.h>
 
 TEST(Person, NoProductionIfNoResources)
 {
+    UpdateContext uc;
+    uc.mObjectiveUtilities = GetObjectiveUtilities();
+    uc.mProductions = GetProductions();
     auto person = std::shared_ptr<Person>(new Person(3000, 0, {}));
+    EXPECT_EQ(person->GetBestProduction(uc), kInvalidId);
 }
 
 TEST(Person, NoProductionIfNoTools)
