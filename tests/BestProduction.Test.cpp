c06dc95345030898f1b16bba32b5a9a4a2f87960
barsandcat <barsandcat@gmail.com>
2020-12-22 20:59:09 +0200

    NoProductionIfDoesNotLikeOutput works

--- tests/BestProduction.Test.cpp
+++ tests/BestProduction.Test.cpp
@@ -7,37 +7,38 @@
 
 TEST_F(UpdateContextTestFixture, NoProductionIfNoResources)
 {
-    auto person = std::shared_ptr<Person>(new Person(3000, 0, {}));
-    EXPECT_EQ(person->GetBestProduction(uc), kInvalidId);
+	auto person = std::shared_ptr<Person>(new Person(3000, 0, {}));
+	EXPECT_EQ(person->GetBestProduction(uc), kInvalidId);
 }
 
 TEST_F(UpdateContextTestFixture, NoProductionIfHasEnough)
 {
-    auto settlement = std::shared_ptr<Settlement>(new Settlement());
-    auto person = std::shared_ptr<Person>(new Person(3000, 80, {}));
-    settlement->AddPerson(person);
-    auto farm = std::shared_ptr<Farm>(new Farm(10000));
-    settlement->AddBuilding(farm);
-    person->ClaimFarm(farm);
-    for (int i = 0; i < 20; i++)
-    {
-        auto food = std::shared_ptr<Food>(new Food(500));
-        settlement->AddFood(food);
-        person->ClaimFood(food);
-    }
-    
-    EXPECT_EQ(person->GetBestProduction(uc), kInvalidId);
-}
+	auto settlement = std::shared_ptr<Settlement>(new Settlement());
+	auto person = std::shared_ptr<Person>(new Person(3000, 80, {}));
+	settlement->AddPerson(person);
+	auto farm = std::shared_ptr<Farm>(new Farm(10000));
+	settlement->AddBuilding(farm);
+	person->ClaimFarm(farm);
+	for (int i = 0; i < 20; i++)
+	{
+		auto food = std::shared_ptr<Food>(new Food(500));
+		settlement->AddFood(food);
+		person->ClaimFood(food);
+	}
 
-TEST_F(UpdateContextTestFixture, NoProductionIfNoTools)
-{
+	EXPECT_EQ(person->GetBestProduction(uc), kInvalidId);
 }
-
 
 TEST_F(UpdateContextTestFixture, NoProductionIfDoesNotLikeOutput)
 {
+	auto person = std::shared_ptr<Person>(new Person(3000, 80, {{ kFarmId, 0.01f }, { kFoodId, 0.01f }}));
+	EXPECT_EQ(person->GetBestProduction(uc), kScavengeId);
 }
 
 TEST_F(UpdateContextTestFixture, ProductionIfDoesNotLikeOutputButHasNotEnough)
+{
+}
+
+TEST_F(UpdateContextTestFixture, NoProductionIfNoTools)
 {
 }
