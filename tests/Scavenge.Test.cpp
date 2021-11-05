3b309327d19054afaa4f6d4e345c682639dd7b3d
barsandcat <>
2021-11-05 16:22:22 +0200

    Lost file

--- tests/Scavenge.Test.cpp
+++ tests/Scavenge.Test.cpp
@@ -12,12 +12,12 @@ TEST_F(UpdateContextTestFixture, ScavengeFood)
 	Person* person1 = new Person(3000, 0, {});
 	Food* food1 = new Food(100);
 	person1->Add(kFoodId, std::unique_ptr<Item>(food1));
-	settlement.GetLand(0)->GetNothing().AddOwner(std::unique_ptr<Person>(person1));
+	settlement.GetLand(0)->GetNullBuilding().AddOwner(std::unique_ptr<Person>(person1));
 
 	Person* person2 = new Person(3000, 0, {});
-	settlement.GetLand(0)->GetNothing().AddOwner(std::unique_ptr<Person>(person2));
+	settlement.GetLand(0)->GetNullBuilding().AddOwner(std::unique_ptr<Person>(person2));
 	Food* food2 = new Food(100);
-	settlement.GetLand(0)->GetNothing().GetNobody().Add(kFoodId, std::unique_ptr<Item>(food2));
+	settlement.GetLand(0)->GetNullBuilding().GetNullOwner().Add(kFoodId, std::unique_ptr<Item>(food2));
 
 	person2->Produce(uc, kScavengeId);
 	EXPECT_EQ(food1->GetOwner(), person1);
